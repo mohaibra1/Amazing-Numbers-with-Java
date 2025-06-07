7b3731a5b4b50f3b6ec9e0603ffb7be7ebef5af6
Moha <mimohadinho@gmail.com>
2025-06-07 10:00:43 +0200

    Stage 5/8: spy numbers

--- Amazing Numbers/task/src/numbers/Numbers.java
+++ Amazing Numbers/task/src/numbers/Numbers.java
@@ -13,98 +13,120 @@ public class Numbers {
         while(true) {
             System.out.println("Enter a request: ");
             String input = scanner.nextLine();
+
             if (Objects.equals(input, "0")){
                 System.out.println("Goodbye!");
                 break;
             }
-            String[] splitted = input.split(" ");
-            if(splitted.length == 1){
-                properties(input);
-            }else if(splitted.length == 2){
-                processConsecutive(splitted);
+            String[] strings = input.split(" ");
+            try {
+                handleInput(strings);
+            }catch (Exception ex){
+                System.out.println(ex.getMessage());
+                continue;
             }
+
+            if(strings.length == 1){
+                properties(input);
+            }else {
+                processConsecutive(strings);
+            }
+//            else if(strings.length == 3){
+//                processConsecutiveWithProperty(strings);
+//            }
+        }
+    }
+
+    private void handleInput(String[] strings) throws Exception {
+        List<String> list = List.of("EVEN","ODD", "BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY");
+        if (Long.parseLong(strings[0]) < 0) {
+            throw new Exception("The first parameter should be a natural number or zero.");
+        }if(strings.length > 1 && Long.parseLong(strings[1]) < 0) {
+            throw new Exception("The second parameter should be a natural number");
+        }if (strings.length > 2 && !list.contains(strings[2])){
+            throw new Exception("The property [" + strings[2] + "] is wrong. \nAvailable properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]");
         }
     }
 
     private void processConsecutive(String[] inputs){
-        try {
             System.out.println();
             long n = Long.parseLong(inputs[0]);
             String temp = inputs[1];
             long increment = Long.parseLong(temp);
-            if (n < 0 || increment < 0){
-                throw new Exception("");
-            }
+
             StringBuilder stringBuilder = new StringBuilder();
             for (long i = 1; i <= increment; i++) {
                 BuzzNumber buzzNumber = new BuzzNumber(n + "");
                 buzzNumber.buzzNumber();
-                DuckNumber duckNumber = new DuckNumber(n + "");
-                boolean isdDuckNumber = duckNumber.isDuckNumber();
 
-                stringBuilder.append(n).append(" is ");
-                if (buzzNumber.isBuzzNumber()) {
-                    stringBuilder.append("buzz, ");
+                String formatted = NumberFormat.getInstance(Locale.US).format(n);
+                String str = n + "";
+
+                String string = formatted + " is " + (buzzNumber.isBuzzNumber()? "buzz, ":"") +
+                        (isDuckNumber(str) ? "duck, ": "") +
+                        (isPalindromic(str)? "palindromic, ": "") +
+                        (isGalpful(str)? "gapful, ": "") +
+                        (isSPY(str)? "spy, ": "") +
+                        (isEven(str)? "even" : "odd") +
+                        ("\n");
+
+                if (inputs.length == 3){
+                    String s = inputs[2].toLowerCase();
+                    if (string.contains(s)){
+                        stringBuilder.append(string);
+                    }else{
+                        i--;
+                    }
+                }else {
+                stringBuilder.append(string);
                 }
-                if (isdDuckNumber) {
-                    stringBuilder.append("duck, ");
-                }
-                if (isPalindromic(n + "")) {
-                    stringBuilder.append("palindromic, ");
-                }
-                if (isGalpful(n + "")) {
-                    stringBuilder.append("gapful, ");
-                }
-                stringBuilder.append((n % 2 == 0 ? "even" : "odd"));
-                stringBuilder.append("\n");
+
                 n++;
             }
 
+
             System.out.println(stringBuilder);
-        }catch (Exception ex){
-            System.out.println("second parameter should be a natural number");
-        }
 
     }
 
+
     private void properties(String input){
-        try {
             //User input
             long check = Long.parseLong(input);
-            if (check < 0) {
-                throw new Exception("");
-            }
             BuzzNumber buzz = new BuzzNumber(input);
             buzz.buzzNumber();
-            DuckNumber duckNumber = new DuckNumber(input);
 
             String formatted = NumberFormat.getInstance(Locale.US).format(check);
 
-            System.out.println("Properties of " + formatted);
-            System.out.println("        even: " + (check % 2 == 0));
-            System.out.println("          odd: " + (check % 2 != 0));
-            System.out.println("        buzz: " + buzz.isBuzzNumber());
-            System.out.println("        duck: " + duckNumber.isDuckNumber());
-            System.out.println("        gapful: " + isGalpful(input));
-            System.out.println("palindromic: " + isPalindromic(input));
-            System.out.println();
-        } catch (Exception ex) {
-            System.out.println("The first parameter should be a natural number or zero.");
-            System.out.println();
-        }
+        String stringBuilder = "Properties of " + formatted + "\n" +
+                "        even: " + isEven(check + "") + "\n" +
+                "          odd: " + (check % 2 != 0) + "\n" +
+                "        buzz: " + buzz.isBuzzNumber() + "\n" +
+                "        duck: " + isDuckNumber(input) + "\n" +
+                "        gapful: " + isGalpful(input) + "\n" +
+                "        spy: " + (isSPY(input) + "\n" +
+                "palindromic: " + isPalindromic(input) + "\n");
+
+        System.out.println(stringBuilder);
     }
 
     private void welcomeNote(){
         System.out.println("Welcome to Amazing Numbers!");
         System.out.println();
         System.out.println("""
-    Supported requests:
-    -enter a natural number to know its properties;
-    -enter two natural numbers to obtain the properties of the list:
-        * the first parameter represents a starting number;
-        * the second parameter shows how many consecutive numbers are to be printed;
-    -separate the parameters with one space;
-    -enter 0 to exit.""");
+Supported requests:
+ - enter a natural number to know its properties;
+ - enter two natural numbers to obtain the properties of the list:
+   * the first parameter represents a starting number;
+   * the second parameter shows how many consecutive numbers are to be printed;
+ - two natural numbers and a property to search for;
+ - separate the parameters with one space;
+ - enter 0 to exit.""");
+    }
+
+
+    private boolean isDuckNumber(String duckNumber){
+        return duckNumber.matches(".*0.*");
     }
 
     private boolean isPalindromic(String palindorom){
@@ -119,11 +141,39 @@ public class Numbers {
         }
         String first = String.valueOf(gapful.charAt(0));
         String last = String.valueOf(gapful.charAt(gapful.length()-1));
-        String joinedstr = first + last;
+        String disjointed = first + last;
 
         long x = Long.parseLong(gapful);
-        long y = Long.parseLong(joinedstr);
+        long y = Long.parseLong(disjointed);
 
         return x % y == 0;
+    }
+
+    private boolean isSPY(String spy){
+        String[] spys = spy.split("");
+        long add = add(spys);
+        long multiply = multiply(spys);
+
+        return add == multiply;
+    }
+
+    private boolean isEven(String s){
+        return Long.parseLong(s) % 2 == 0;
+    }
+
+    private long add(String[] spys){
+        long result = 0;
+        for (String s: spys){
+            result += Long.parseLong(s);
+        }
+        return result;
+    }
+
+    private long multiply(String[] spys){
+        long result = 1;
+        for (String s: spys){
+            result *= Long.parseLong(s);
+        }
+        return result;
     }
 }
